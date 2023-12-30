27cfbe6e7021482eecb55af5bbfd1ae606465740
blissartt <146645211+blissartt@users.noreply.github.com>
2023-12-30 20:10:35 +0100

    Update Samsung-Galaxy-A3-DebloatBasic-ARCH.sh

--- Samsung-Galaxy-A3-DebloatBasic-ARCH.sh
+++ Samsung-Galaxy-A3-DebloatBasic-ARCH.sh
@@ -14,13 +14,12 @@ else
 fi
 
 
-#echo "Setting up Android SKDs..."
-#sudo apt install android-tools-adb android-tools-fastboot
-#sudo apt install lolcat
-#sleep 2
-#echo "ADB was set up! 
+echo "Setting up Android SKDs..."
+sudo pacman install android-tools
+sleep 2
+echo "ADB was set up! 
 
-#" | lolcat
+" | lolcat
 
 
 echo "Starting ADB... This will ask you for on-device confirmation.
